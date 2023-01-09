2255f809b7f30d4b2e4ffe9475f7bd02ae11b904
Hope Kagame <kagame458@protonmail.com>
2023-01-09 13:43:47 +0300

    Ignore print error

--- server/routes/ws.dart
+++ server/routes/ws.dart
@@ -3,6 +3,7 @@ import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
 
 Handler get onRequest {
   return webSocketHandler((channel, protocol) {
+    // ignore: avoid_print
     channel.stream.listen(print);
 
     // Send a message back to the client.
